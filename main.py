d826253058f32ea712b07770fcc463f509727604
Aziz Alfarisi <az.alfarisii@gmail.com>
2026-02-03 08:52:32 +0700

    Add error-handling using yesterday's data when today's data hasn't been updated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Astronomy Picture of the Day
 import streamlit as st
-import datetime as dt
+from datetime import date, timedelta
 from sqlalchemy import text
 from dotenv import load_dotenv
 import requests
@@ -8,29 +8,36 @@ import os
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
-today = dt.date.today().isoformat()
+today = date.today().isoformat()
 
 # Connect with SQL Database
 conn = st.connection('apod_db', type='sql')
 query = "SELECT * FROM apod_table WHERE date = :today;"
-data = conn.query(query, params={'today': '2026-02-01'}, ttl=0)
+data = conn.query(query, params={'today': today}, ttl=0)
 
 if not data.empty:
     # Fetch the data straight from the database
     content = data.iloc[0].to_dict()
 else:
-    # Request today's APOD data from NASA's API
-    url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}&date=today"
-    response = requests.get(url)
-    content = response.json()
+    try:
+        # Request today's APOD data from NASA's API
+        url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}&date={today}"
+        response = requests.get(url)
+        content = response.json()
 
-    # Update the database with today's APOD
-    with conn.session as s:
-        s.execute(text("CREATE TABLE IF NOT EXISTS apod_table (date TEXT, title TEXT, url TEXT, explanation TEXT);"))
-        s.execute(text("INSERT INTO apod_table VALUES (:date, :title, :url, :explanation);"),
-                  params={'date': today, 'title': content['title'],
-                          'url': content['url'], 'explanation': content['explanation']})
-        s.commit()
+        # Update the database with today's APOD
+        with conn.session as s:
+            s.execute(text("CREATE TABLE IF NOT EXISTS apod_table (date TEXT, title TEXT, url TEXT, explanation TEXT);"))
+            s.execute(text("INSERT INTO apod_table VALUES (:date, :title, :url, :explanation);"),
+                      params={'date': today, 'title': content['title'],
+                              'url': content['url'], 'explanation': content['explanation']})
+            s.commit()
+    except KeyError:
+        # Fetch yesterday's data if today's data is not available
+        query = "SELECT * FROM apod_table WHERE date = :yesterday;"
+        data = conn.query(query, params={'yesterday': date.today() - timedelta(days=1)}, ttl=0)
+        content = data.iloc[0].to_dict()
+
 
 # Web Development
 st.title(content['title'])

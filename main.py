d3cb9fc1d4e936db528a708adfd9df88686a5ae4
Aziz Alfarisi <az.alfarisii@gmail.com>
2026-02-03 18:43:28 +0700

    Add date input to browse through previous APODs up to 30 days

--- main.py
+++ main.py
@@ -4,31 +4,33 @@ from datetime import date, timedelta
 
 # Connect with SQL Database
 conn = st.connection('apod_db', type='sql')
-today = date.today().isoformat()
-data = b.fetch_db(conn, today)
+today = date.today()
 
+# Date Input
+st.subheader("Astronomy Picture of The Day")
+day = st.date_input("", today, label_visibility="collapsed", format="DD.MM.YYYY",
+                    max_value=today, min_value=today - timedelta(days=30))
+
+# Fetch APOD data straight from the database
+data = b.fetch_db(conn, day)
 if not data.empty:
-    # Fetch APOD data straight from the database
     content = data.iloc[0].to_dict()
 else:
     try:
-        # Request today's APOD data from NASA's API
-        content = b.request_api(today)
-
-        # Insert today's APOD data to the database
+        # Request APOD data from NASA's API (If not present in database) and insert it to the database
+        content = b.request_api(day)
         b.insert_db(conn, content)
     except KeyError:
         # Fetch yesterday's data if today's data is not available
-        yesterday = (date.today() - timedelta(days=1)).isoformat()
+        yesterday = (today - timedelta(days=1)).isoformat()
         data = b.fetch_db(conn, yesterday)
         content = data.iloc[0].to_dict()
 
 # Change date format (Ex: 2026-02-01 -> Sunday, 01 February 2026)
 content['date'] = date.strptime(content['date'], '%Y-%m-%d').strftime('%A, %d %B %Y')
 
-# Web Development
+# Show APOD (Web Development)
 st.title(content['title'])
-st.subheader("Astronomy Picture of The Day")
-st.text(content['date'])
+st.write(content['date'])
 st.image(content['url'])
 st.write(content['explanation'])

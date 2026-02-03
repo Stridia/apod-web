fef417d467988455b4243c600c7fa703aa5ff794
Aziz Alfarisi <az.alfarisii@gmail.com>
2026-02-03 09:23:16 +0700

    Add backend for frontend functions to improve code readability

---
+++ backend.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+import requests, os
+import streamlit as st
+from sqlalchemy import text
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+
+def request_api(day):
+    """Request APOD data on a certain date from NASA's API"""
+    url = f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}&date={day}"
+    response = requests.get(url)
+    return response.json()
+
+def fetch_db(connection, day):
+    """Fetch APOD record on a certain date from the local database using query"""
+    query = "SELECT * FROM apod_table WHERE date = :date;"
+    data = connection.query(query, params={'date': day}, ttl=0)
+    return data
+
+def update_db(connection, content):
+    """Update database by adding new APOD data from NASA's API"""
+    with connection.session as s:
+        s.execute(text("CREATE TABLE IF NOT EXISTS apod_table (date TEXT, title TEXT, url TEXT, explanation TEXT);"))
+        s.execute(text("INSERT INTO apod_table VALUES (:date, :title, :url, :explanation);"),
+                  params={'date': content['date'], 'title': content['title'],
+                          'url': content['url'], 'explanation': content['explanation']})
+        s.commit()
+
+def printall_db(connection):
+    """Print all recorded APOD data in the local database"""
+    data = connection.query("SELECT * FROM apod_table ORDER BY date DESC", ttl=0)
+    print(data)
+
+def delete_db(connection, day):
+    """Delete APOD record from the local database"""
+    with connection.session as s:
+        s.execute(text("DELETE FROM apod_table WHERE date = :day"), params={'day': day})
+        s.commit()
+
+
+if __name__ == '__main__':
+    conn = st.connection('apod_db', type='sql')
+    printall_db(conn)
--- main.py
+++ main.py
@@ -1,41 +1,26 @@
-# Astronomy Picture of the Day
 import streamlit as st
+import backend as b
 from datetime import date, timedelta
-from sqlalchemy import text
-from dotenv import load_dotenv
-import requests
-import os
-
-load_dotenv()
-api_key = os.getenv("API_KEY")
-today = date.today().isoformat()
 
 # Connect with SQL Database
 conn = st.connection('apod_db', type='sql')
-query = "SELECT * FROM apod_table WHERE date = :today;"
-data = conn.query(query, params={'today': today}, ttl=0)
+today = date.today().isoformat()
+data = b.fetch_db(conn, today)
 
 if not data.empty:
-    # Fetch the data straight from the database
+    # Fetch APOD data straight from the database
     content = data.iloc[0].to_dict()
 else:
     try:
         # Request today's APOD data from NASA's API
-        url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}&date={today}"
-        response = requests.get(url)
-        content = response.json()
+        content = b.request_api(today)
 
         # Update the database with today's APOD
-        with conn.session as s:
-            s.execute(text("CREATE TABLE IF NOT EXISTS apod_table (date TEXT, title TEXT, url TEXT, explanation TEXT);"))
-            s.execute(text("INSERT INTO apod_table VALUES (:date, :title, :url, :explanation);"),
-                      params={'date': today, 'title': content['title'],
-                              'url': content['url'], 'explanation': content['explanation']})
-            s.commit()
+        b.update_db(conn, content)
     except KeyError:
         # Fetch yesterday's data if today's data is not available
-        query = "SELECT * FROM apod_table WHERE date = :yesterday;"
-        data = conn.query(query, params={'yesterday': date.today() - timedelta(days=1)}, ttl=0)
+        yesterday = (date.today() - timedelta(days=1)).isoformat()
+        data = b.fetch_db(conn, yesterday)
         content = data.iloc[0].to_dict()
 
 
